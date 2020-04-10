5b21b518b2a58b1ea1a5b653caa61aee53428776
Jordan Nielson <jnielson94@gmail.com>
2020-04-09 22:01:20 -0600

    Add process-url

---
+++ functions/process-url/process-url.js
@@ -0,0 +1,45 @@
+const cloudinary = require("cloudinary").v2;
+const qs = require("querystring");
+cloudinary.config({
+    cloud_name: "jnielson",
+    api_key: process.env.CLOUDINARY_KEY,
+    api_secret: process.env.CLOUDINARY_SECRET
+})
+
+exports.handler = async function (event, ctx) {
+  const {queryStringParameters} = event
+  console.log(queryStringParameters)
+  try {
+    // https://res.cloudinary.com/sector/image/upload/v1583637123/og-images/img-1.png
+    const imageUrl = cloudinary.url(
+      `${process.env.IMAGE_VERSION}/og-images/transparent.png`,
+      {
+        // resouce_type: "raw"
+        sign_url: true,
+        // secure: true,
+        custom_pre_function: {
+          function_type: 'remote',
+          source: encodeURIComponent(
+            `https://agitated-edison-8b447e.netlify.com/.netlify/functions/gen-opengraph-image?${qs.stringify(
+              queryStringParameters,
+            )}`,
+          ),
+        },
+      },
+    )
+    console.log(
+      `https://agitated-edison-8b447e.netlify.com/.netlify/functions/gen-opengraph-image?${qs.stringify(
+        queryStringParameters,
+      )}`,
+    )
+    return {
+      statusCode: 302,
+      headers: {
+        Location: imageUrl,
+      },
+      body: '',
+    }
+  } catch (e) {
+    console.log(e)
+  }
+}
