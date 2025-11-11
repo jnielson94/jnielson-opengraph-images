a5a0cf972779b334d6086417cc33117c7ff8f771
Jordan Nielson <jnielson94@gmail.com>
2025-11-11 00:28:38 -0500

    ENV variable for gen function so it isn't always prod

--- functions/process-url/process-url.js
+++ functions/process-url/process-url.js
@@ -12,21 +12,21 @@ exports.handler = async function (event, ctx) {
 
   try {
     // https://res.cloudinary.com/sector/image/upload/v1583637123/og-images/img-1.png
-    const imageUrl = cloudinary.url(
-      `${process.env.IMAGE_VERSION}/og-images/transparent.png`,
-      {
-        // resouce_type: "raw"
-        sign_url: true,
-        secure: true,
-        custom_pre_function: {
-          function_type: "remote",
-          source: `https://jordans-images.netlify.app/.netlify/functions/gen-opengraph-image?${qs.stringify(
-            queryStringParameters
-          )}`,
-        },
-        fetch_format: "png",
-      }
-    );
+    const transparentURL = `${process.env.IMAGE_VERSION}/og-images/transparent.png`;
+    const baseURL =
+      process.env.GEN_OPENGRAPH_IMAGE_BASE_URL ||
+      "https://jordans-images.netlify.app/.netlify/functions/gen-opengraph-image";
+    console.log("URLs: ", { transparentURL, baseURL });
+    const imageUrl = cloudinary.url(transparentURL, {
+      // resouce_type: "raw"
+      sign_url: true,
+      secure: true,
+      custom_pre_function: {
+        function_type: "remote",
+        source: `${baseURL}?${qs.stringify(queryStringParameters)}`,
+      },
+      fetch_format: "png",
+    });
 
     console.log({
       params: `${qs.stringify(queryStringParameters)}`,
